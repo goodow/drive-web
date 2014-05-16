fcdda669838a369551a037c7049861fd4975546b
leiguorui <leiguorui@goodow.com>
2014-05-16 14:25:17 +0800

    前端连接指定后台server
    Change-Id: If79d3c439fd9143e013300c9a5ea6097b1f939aa

--- app/js/app.js
+++ app/js/app.js
@@ -3,22 +3,33 @@
 // Declare app level module which depends on filters, and services
 
 angular.module('drive', [
-    'ngRoute',
-    'good.ui.grid',
-    'drive.controllers',
-    'drive.services',
-    'drive.directives',
-    'drive.filters'])
-.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider) {
+  'ngRoute',
+  'good.ui.grid',
+  'drive.controllers',
+  'drive.services',
+  'drive.directives',
+  'drive.filters',
+  'ngCookies'])
+    .config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider) {
 //  $locationProvider.html5Mode(true).hashPrefix('');
-  $routeProvider
-      .when('/datagrid/attachment', {templateUrl: 'partials/data-grid.html',controller:'AttachmentCtrl'})
-      .when('/datagrid/attachmentActivity', {templateUrl: 'partials/data-grid.html',controller:'AttachmentActivityCtrl'})
-      .when('/datagrid/device', {templateUrl: 'partials/data-grid.html',controller:'DeviceCtrl'})
-      .when('/datagrid/deviceActivity', {templateUrl: 'partials/data-grid.html',controller:'DeviceActivityCtrl'})
-      .otherwise({'redirectTo':'/index.html' });
-}])
+//      $locationProvider.html5Mode(true);
+      $routeProvider
+          .when('/datagrid/attachment', {templateUrl: 'partials/data-grid.html',controller:'AttachmentCtrl'})
+          .when('/datagrid/attachmentActivity', {templateUrl: 'partials/data-grid.html',controller:'AttachmentActivityCtrl'})
+          .when('/datagrid/device', {templateUrl: 'partials/data-grid.html',controller:'DeviceCtrl'})
+          .when('/datagrid/deviceActivity', {templateUrl: 'partials/data-grid.html',controller:'DeviceActivityCtrl'});
+    }])
 //
-//.run(['$templateCache',function($templateCache){
+.run(['$templateCache','$location','$cookieStore','$cookies','Constant',function($templateCache,$location,$cookieStore,$cookies,Constant){
+      var searchObject =  $location.hash()||$location.$$path;
+      if(searchObject&&searchObject.indexOf('server')>=0){
+        var serverUrl = searchObject.substring(searchObject.indexOf('=')+1);
+        $cookieStore.put('server',serverUrl);
+      }
+      var serverUrl = $cookieStore.get('server');
+      if(serverUrl){
+        Constant.serverUrl = serverUrl;
+      }
+      console.log(serverUrl);
 //    $templateCache.put('partials/helloworld1.html',"<h1 style='color:blue;'>{{helloworld1}}</h1>");
-//}]);
+}]);
