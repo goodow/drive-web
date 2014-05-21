d15495563e2b79aa8510153fdca35e650053fa95
leiguorui <leiguorui@goodow.com>
2014-05-21 16:06:54 +0800

    修改查询设备操作管理的查询条件和排序
    Change-Id: Ie839eb7ceba153927fdff621600895476c00c57a

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -176,7 +176,7 @@ angular.module('drive.controllers',[])
             '_type':type,
             source:{
               sort:[
-                {'open':"desc"},
+//                {'open':"desc"},
                 '_uid'
               ],
               'size':size
@@ -212,7 +212,7 @@ angular.module('drive.controllers',[])
           queryString =  {
             "multi_match" : {
               "query" : '"'+keyword+'"',
-              "fields" : ["deviceId", "owner^2"]
+              "fields" : ["deviceId","address", "user^2"]
             }
           }
         }
@@ -236,6 +236,7 @@ angular.module('drive.controllers',[])
           $scope.datas = datas;
         });
       };
+      $log.log(JSON.stringify(searchParam));
       bus().send(Constant.search_channel,searchParam,callback);
       $scope.prePage = function(){
         if(currentPage <= 1){
@@ -336,7 +337,7 @@ angular.module('drive.controllers',[])
             '_type':type,
             source:{
               sort:[
-                {"open":"desc"},
+//                {"open":"desc"},
                 "_uid"
               ],
               'size':size
