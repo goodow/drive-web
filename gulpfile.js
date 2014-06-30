c6adcba883636eefa94c6d7dd466e5f001f1394d
Benjamin <shangmin@goodow.com>
2014-06-30 10:54:02 +0800

    modify travis.yml,gulpfile.js refator auto build
    Change-Id: I023795b9ace6ca423b525c6da3e6795e4c329a02

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,60 @@
 var gulp = require('gulp');
-
-
+var $ = require('gulp-load-plugins')();
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var cssmin = require('gulp-cssmin');
+var del = require('del');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
 
 var paths={
   scripts:['app/js/*.js'],
-  css:'app/css/*.css'
+  css:'app/css/*.css',
+  images:'app/img/*'
 };
+// Lint JavaScript
+gulp.task('jshint', function () {
+    return gulp.src(paths.scripts)
+        .pipe(reload({stream: true, once: true}))
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+});
+// Optimize Images
+gulp.task('images', function () {
+    return gulp.src(paths.images)
+        .pipe($.cache($.imagemin({
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('app/build/images'))
+        .pipe($.size({title: 'images'}));
+});
 gulp.task('uglify',function(){
   return gulp.src(paths.scripts)
     .pipe(uglify())
     .pipe(concat('all.min.js'))
-    .pipe(gulp.dest('build/js'));
+    .pipe(gulp.dest('app/build/js'));
 });
 gulp.task('concat',function(){
   return gulp.src(paths.scripts)
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('build/js'));
+    .pipe(gulp.dest('app/build/js'));
 });
 gulp.task('cssmin',function(){
   return gulp.src(paths.css)
-    .pipe(concat('all.js'))
-    .pipe(gulp.dest('build/css'));
+    .pipe(concat('all.css'))
+    .pipe(gulp.dest('app/build/css'));
 });
+gulp.task('serve',function() {
+    browserSync({
+        notify: false,
+        server: {
+            baseDir: ['app']
+        }
+    });
+});
+
 gulp.task('default',['uglify','concat','cssmin']);
 
